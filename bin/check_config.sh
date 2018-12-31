cbf5b812ca700172e7ae3b1dfc2f999caeede5de
Sean Kelly <smkelly@smkelly.org>
2018-12-30 23:17:40 -0600

    whitespace fixes

--- bin/check_config.sh
+++ bin/check_config.sh
@@ -9,6 +9,6 @@ VERSION=$(cat homeassistant/.HA_VERSION)
 docker run --rm -it \
 	--name hass_test \
 	-v ${SSL_PATH}:/etc/ssl/private:ro \
-       	-v /opt/smarthome/homeassistant/:/config:ro \
-       	homeassistant/home-assistant:${VERSION} \
+	-v /opt/smarthome/homeassistant/:/config:ro \
+	homeassistant/home-assistant:${VERSION} \
 	python -m homeassistant -c /config --script check_config
